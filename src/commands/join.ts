01e48e00c4d6abd2435e0411ec5f55a971e75c79
Junius Khuong <scottkhuong@gmail.com>
2023-02-12 15:09:58 +0700

    Create join command

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -1,4 +1,8 @@
+import { joinVoiceChannel } from "@discordjs/voice";
 import { Message } from "discord.js";
+
+import responseSamples from "./randomResponseCollection.json";
+import { checkInVoiceChannel } from "./utils";
 
 import { GeneralCommand } from "../types";
 
@@ -6,6 +10,16 @@ export const joinCommand: GeneralCommand = {
   type: "general",
   name: "join",
   run: async (message: Message) => {
-    console.log(message);
+    if (!checkInVoiceChannel(message, responseSamples.joinCommand.failed)) {
+      return;
+    }
+
+    if (message.member?.voice.channel) {
+      joinVoiceChannel({
+        channelId: message.member?.voice.channel.id,
+        guildId: message.member?.voice.channel.guildId,
+        adapterCreator: message.member?.voice.channel.guild.voiceAdapterCreator,
+      });
+    }
   },
 };
