30fa91f1cde4480dfd8a94862f7a874fe248d4e2
Junius Khuong <scottkhuong@gmail.com>
2023-02-19 20:37:03 +0700

    Update and create new utility functions

--- src/utilities/commands/musicCommands.ts
+++ src/utilities/commands/musicCommands.ts
@@ -1,6 +1,13 @@
 import { AudioResource, createAudioResource } from "@discordjs/voice";
+import SpotifyFetcher from "spotifydl-core/dist/Spotify";
 import ytdl from "ytdl-core";
 
+import { Readable } from "stream";
+
+import {
+  BotHomemadeGeneralState,
+  BotHomemadeMusicStateManager,
+} from "../../StateManager";
 import { Song } from "../../types";
 
 export const generateAudioStream = (link: string): AudioResource => {
@@ -14,6 +21,23 @@ export const generateAudioStream = (link: string): AudioResource => {
   }
 
   const resource = createAudioResource(track, {
+    inlineVolume: true,
+  });
+
+  return resource;
+};
+
+export const generateSpotifyAudioStream = async (
+  trackUrl: string
+): Promise<AudioResource<unknown> | undefined> => {
+  const spotifyDownloader = BotHomemadeMusicStateManager.spotify
+    .downloaderInstance as SpotifyFetcher;
+
+  const downloadResult = await spotifyDownloader.downloadTrack(trackUrl);
+
+  if (!downloadResult) return;
+
+  const resource = createAudioResource(Readable.from(downloadResult), {
     inlineVolume: true,
   });
 
@@ -40,4 +64,14 @@ export const createSongObject = (
   };
 
   return song;
+};
+
+export const playSong = async (songUrl: string) => {
+  if (ytdl.validateURL(songUrl)) {
+    BotHomemadeGeneralState.audioPlayer.play(generateAudioStream(songUrl));
+  } else {
+    BotHomemadeGeneralState.audioPlayer.play(
+      (await generateSpotifyAudioStream(songUrl)) as AudioResource<unknown>
+    );
+  }
 };
