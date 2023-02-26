674014c48002cc073f69266d3463cb16a0c90746
huykhuong <46696683+huykhuong@users.noreply.github.com>
2023-02-26 11:11:58 +0700

    Hot fix LCK and small upgrades (#39)

--- src/commands/lolSchedule/lck.ts
+++ src/commands/lolSchedule/lck.ts
@@ -52,7 +52,7 @@ const lckCommand: GeneralCommand = {
             {
               style: 5,
               label: `Watch now on LCK Tieng Viet`,
-              url: `https://www.youtube.com/@LCKglobal`,
+              url: `https://www.youtube.com/@LCKTiengViet`,
               disabled: false,
               type: 2,
             },
@@ -81,7 +81,8 @@ export default lckCommand;
 
 const getMatchStatus = (timeOfMatch: number, timeNowToday: number): string => {
   if (
-    Math.ceil((timeOfMatch - timeNowToday) / 1000 / 60) >= -150 ||
+    (Math.ceil((timeOfMatch - timeNowToday) / 1000 / 60) >= -150 &&
+      Math.ceil((timeOfMatch - timeNowToday) / 1000 / 60) < 0) ||
     Math.ceil((timeOfMatch - timeNowToday) / 1000 / 60) === 0
   ) {
     return "Live now 🔴";
@@ -89,9 +90,7 @@ const getMatchStatus = (timeOfMatch: number, timeNowToday: number): string => {
     return "Match concluded";
   }
 
-  return `Live in ${Math.floor(
-    Math.ceil((timeOfMatch - timeNowToday) / 1000 / 60)
-  )}`;
+  return `Live in ${formatTime(timeOfMatch - timeNowToday)}`;
 };
 
 const insertVSWord = (matchName: string): string => {
@@ -100,4 +99,24 @@ const insertVSWord = (matchName: string): string => {
   match.splice(1, 0, "vs");
 
   return match.join(" ");
+};
+
+const formatTime = (miliseconds: number): string => {
+  const minutes = miliseconds / 1000 / 60;
+
+  const hours = minutes / 60;
+
+  const roundedDownHours = Math.floor(hours);
+
+  const leftoverMinutes = Math.round((hours - roundedDownHours) * 60);
+
+  if (roundedDownHours <= 1 && leftoverMinutes <= 1) {
+    return `${roundedDownHours} hour and ${leftoverMinutes} minute`;
+  } else if (roundedDownHours <= 1 && leftoverMinutes > 1) {
+    return `${roundedDownHours} hour and ${leftoverMinutes} minutes`;
+  } else if (roundedDownHours > 1 && leftoverMinutes <= 1) {
+    return `${roundedDownHours} hours and ${leftoverMinutes} minute`;
+  }
+
+  return `${roundedDownHours} hours and ${leftoverMinutes} minutes`;
 };
