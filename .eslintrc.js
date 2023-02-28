b47292191b9cf7f2944a072fce800d8b00523ab4
Junius Khuong <scottkhuong@gmail.com>
2023-02-28 23:05:27 +0700

    Update typescript linting rules in eslintc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,5 +27,30 @@ module.exports = {
         "newlines-between": "always",
       },
     ],
+    ///////////////////////////////////////////////////////////////////////////
+    // TypeScript Linting                                                    //
+    ///////////////////////////////////////////////////////////////////////////
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/consistent-type-assertions": "off",
+    "@typescript-eslint/consistent-type-definitions": ["off", "type"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "error",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/member-ordering": "off",
+    "@typescript-eslint/method-signature-style": "error",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-invalid-this": "error",
+    "@typescript-eslint/no-invalid-void-type": "error",
+    "@typescript-eslint/no-unused-expressions": ["error"],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
   },
 };
